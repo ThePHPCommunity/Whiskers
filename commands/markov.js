a5444aac0c5cfdfe531d563c9cec2b13171c7d5b
Richard Bowey <unconfigured@null.spigotmc.org>
2019-05-17 20:08:31 +0100

    Fixed spelling mistake

--- commands/markov.js
+++ commands/markov.js
@@ -15,6 +15,6 @@ commands.msize = {
 	description: "Tells you the size of the markov file currently",
 	usage: prefix + "msize",
 	execute: function(command, message, data) {
-		toConsole('bot', '', data.channel, "```Current Markov Brian\n  File Size: "+cmd_exec("ls -lh markov.txt | awk '{print $5}'").toString().trim()+"\n  Total Lines: "+cmd_exec("cat markov.txt | wc -l").toString().trim()+"\n  Total Words: "+cmd_exec("cat markov.txt | wc -w").toString().trim()+"\n  Unique Words "+cmd_exec("cat markov.txt | tr -s ' ' '\n' | sort | uniq -c | wc -l").toString().trim()+"```");
+		toConsole('bot', '', data.channel, "```Current Markov Brain\n  File Size: " + cmd_exec("ls -lh markov.txt | awk '{print $5}'").toString().trim() + "\n  Total Lines: " + cmd_exec("cat markov.txt | wc -l").toString().trim() + "\n  Total Words: " + cmd_exec("cat markov.txt | wc -w").toString().trim() + "\n  Unique Words " + cmd_exec("cat markov.txt | tr -s ' ' '\n' | sort | uniq -c | wc -l").toString().trim() + "```");
 	}
 }
